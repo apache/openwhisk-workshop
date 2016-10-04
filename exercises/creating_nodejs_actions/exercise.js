3e60fe22ace3e1a793575e27182af42d4a7be49a
James Thomas <james.thomas@uk.ibm.com>
2016-10-04 13:42:09 +0100

    Added NodeJS Actions

---
+++ exercises/creating_nodejs_actions/exercise.js
@@ -0,0 +1,109 @@
+const exercise = require('workshopper-exercise')()
+const shell = require('shelljs')
+
+exercise.requireSubmission = false
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action get hello-world-nodejs', {silent: true}, (code, stdout, stderr) => {
+    const exists = (code === 0)
+    const uses_nodejs = (stdout.match('"kind": "nodejs:6"'))
+
+    const success = exists && !!uses_nodejs
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action hello-world-nodejs exists')
+    } else {
+      this.emit('fail', 'OpenWhisk Action hello-world-nodejs does not exist')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action invoke hello-world-nodejs -b -r', {silent: true}, (code, stdout, stderr) => {
+    const success_code = (code === 0)
+    const found_message = (stdout.match('"message": "Hello World"'))
+
+    const success = success_code && !!found_message
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action hello-world-nodejs returns correct message')
+    } else {
+      this.emit('fail', 'OpenWhisk Action hello-world-nodejs does not return correct message')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action get hello-world-nodejs-delay', {silent: true}, (code, stdout, stderr) => {
+    const exists = (code === 0)
+    const uses_nodejs = (stdout.match('"kind": "nodejs:6"'))
+
+    const success = exists && !!uses_nodejs
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action hello-world-nodejs-delay exists')
+    } else {
+      this.emit('fail', 'OpenWhisk Action hello-world-nodejs-delay does not exist')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action invoke hello-world-nodejs-delay -b', {silent: true}, (code, stdout, stderr) => {
+    const success_code = (code === 0)
+    const found_message = (stdout.match('"message": "Hello World"'))
+
+    const success = success_code && !!found_message
+    if (success) {
+      const start_time = parseInt(stdout.match(/start": (\d+)/)[1], 10)
+      const end_time = parseInt(stdout.match(/end": (\d+)/)[1], 10)
+      if ((end_time - start_time) < 1000) {
+        this.emit('fail', 'OpenWhisk Action hello-world-nodejs-delay returned response within 1000ms.')
+      } else {
+        this.emit('pass', 'OpenWhisk Action hello-world-nodejs-delay returns correct message after delay')
+      }
+    } else {
+      this.emit('fail', 'OpenWhisk Action hello-world-nodejs-delay does not return correct message')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action get london-location-nodejs', {silent: true}, (code, stdout, stderr) => {
+    const exists = (code === 0)
+    const uses_nodejs = (stdout.match('"kind": "nodejs:6"'))
+
+    const success = exists && !!uses_nodejs
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action london-location-nodejs exists')
+    } else {
+      this.emit('fail', 'OpenWhisk Action london-location-nodejs does not exist')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action invoke london-location-nodejs -b -r', {silent: true}, (code, stdout, stderr) => {
+    const success_code = (code === 0)
+    const lat = (stdout.match('"lat": 51.5073509'))
+    const lng = (stdout.match('"lng": -0.1277583'))
+
+    const success = success_code && !!lat && !!lng
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action london-location-nodejs returns correct message')
+    } else {
+      this.emit('fail', 'OpenWhisk Action london-location-nodejs does not return correct message')
+    }
+
+    cb(null, success)
+  })
+})
+
+module.exports = exercise
---
+++ exercises/creating_nodejs_actions/problem.md
@@ -0,0 +1,47 @@
+This exercise will test that you understand how to create OpenWhisk Actions
+using the NodeJS runtime. Complete the following tasks to finish the exercise.
+
+- Create an Action (hello-world-nodejs) which uses NodeJS runtime and returns the following message.
+
+```
+{
+  "message": "Hello World"
+}
+```
+
+- Create an Action (hello-world-delay-nodejs) that returns the same message 
+  after an artificial delay of one second.
+- Create an Action (london-location-nodejs) that uses the Google Geocoding API 
+  to return that latitude and longitude for London in the following format...
+
+```
+{
+  "lat": 0.00,
+  "lng": 0.00
+}
+```
+
+----------------------------------------------------------------------
+## HINTS
+
+Executing functions after a scheduled delay can be achieved using the 
+setTimeout function in NodeJS.
+https://nodejs.org/api/timers.html#timers_settimeout_callback_delay_arg
+
+JavaScript supports Promises for returning asychronous results from functions.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+Documentation for the Google Geocoding API is avaiable here:
+https://developers.google.com/maps/documentation/geocoding/intro
+
+OpenWhisk Actions running in the Node.JS environment have access to popular 
+NPM modules, the full list is available here: 
+https://github.com/openwhisk/openwhisk/blob/master/docs/reference.md#javascript-runtime-environments
+
+This includes the RequestJS module for making HTTP requests.
+https://github.com/request/request
+
+
+----------------------------------------------------------------------
+
+ __»__ To verify your work, run: `openwhisk-workshop verify`
