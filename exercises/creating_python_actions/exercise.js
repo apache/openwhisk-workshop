900729a3c9c186f2f6a98d75863320a03d0ed915
James Thomas <james.thomas@uk.ibm.com>
2016-10-05 15:16:11 +0100

    Adding Python exercise, closes #2

---
+++ exercises/creating_python_actions/exercise.js
@@ -0,0 +1,71 @@
+const exercise = require('workshopper-exercise')()
+const shell = require('shelljs')
+
+exercise.requireSubmission = false
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action get hello-world-python', {silent: true}, (code, stdout, stderr) => {
+    const exists = (code === 0)
+    const uses_python = (stdout.match('"kind": "python"'))
+
+    const success = exists && !!uses_python
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action hello-world-python exists')
+    } else {
+      this.emit('fail', 'OpenWhisk Action hello-world-python does not exist')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action invoke hello-world-python -b -r', {silent: true}, (code, stdout, stderr) => {
+    const success_code = (code === 0)
+    const found_message = (stdout.match('"message": "Hello World"'))
+
+    const success = success_code && !!found_message
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action hello-world-python returns correct message')
+    } else {
+      this.emit('fail', 'OpenWhisk Action hello-world-python does not return correct message')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action get london-location-python', {silent: true}, (code, stdout, stderr) => {
+    const exists = (code === 0)
+    const uses_python = (stdout.match('"kind": "python"'))
+
+    const success = exists && !!uses_python
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action london-location-python exists')
+    } else {
+      this.emit('fail', 'OpenWhisk Action london-location-python does not exist')
+    }
+
+    cb(null, success)
+  })
+})
+
+exercise.addVerifyProcessor(function (cb) {
+  shell.exec('wsk action invoke london-location-python -b -r', {silent: true}, (code, stdout, stderr) => {
+    const success_code = (code === 0)
+    const lat = (stdout.match('"lat": 51.5073509'))
+    const lng = (stdout.match('"lng": -0.1277583'))
+
+    const success = success_code && !!lat && !!lng
+    if (success) {
+      this.emit('pass', 'OpenWhisk Action london-location-python returns correct message')
+    } else {
+      this.emit('fail', 'OpenWhisk Action london-location-python does not return correct message')
+    }
+
+    cb(null, success)
+  })
+})
+
+module.exports = exercise
---
+++ exercises/creating_python_actions/problem.md
@@ -0,0 +1,35 @@
+This exercise will test that you understand how to create OpenWhisk Actions
+using the Python runtime. Complete the following tasks to finish the exercise.
+
+- Create an Action (hello-world-python) which uses Python runtime and returns the following message.
+
+```
+{
+  "message": "Hello World"
+}
+```
+
+- Create an Action (london-location-python) that uses the Google Geocoding API 
+  to return that latitude and longitude for London in the following format...
+
+```
+{
+  "lat": 0.00,
+  "lng": 0.00
+}
+```
+
+----------------------------------------------------------------------
+## HINTS
+
+Make sure your Python files have a top-level function called main.
+
+Documentation for creating Python Actions is available here.
+https://github.com/openwhisk/openwhisk/blob/master/docs/actions.md#creating-python-actions
+
+Documentation for the Google Geocoding API is available here:
+https://developers.google.com/maps/documentation/geocoding/intro
+
+----------------------------------------------------------------------
+
+ __»__ To verify your work, run: `openwhisk-workshop verify`
